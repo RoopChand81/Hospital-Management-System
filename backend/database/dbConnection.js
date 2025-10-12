3c5fcf5eee259a322e135e50d89dd3fb1675c6c3
RoopChand81 <roop46763@gmail.com>
2025-10-13 02:51:16 +0530

    add DBConnection setup

---
+++ backend/database/dbConnection.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const dbConnection = async () => {
+  console.log("Mongo URI:", process.env.MONGO_URI);
+
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error("❌ Error connecting to MongoDB:", error.message);
+    process.exit(1);
+  }
+};
+
+export default dbConnection;
